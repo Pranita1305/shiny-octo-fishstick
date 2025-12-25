eed667777009fdace076d24953c933e0380b6c34
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2025-12-25 12:51:32 +0530

    Time: 159 ms (52.84%) | Memory: 107.7 MB (84.75%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,28 +1,45 @@
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+//         int n=happiness.size();
+
+//         priority_queue<int> pq;
+//         for(int i=0;i<n;i++){
+//             pq.push(happiness[i]);
+//         }
+
+//         int turns=0;
+//         long long happy=0;
+//         while(!pq.empty() && k>0){
+//             int h=pq.top();
+//             if(h-turns>0){
+//                 happy+=(h-turns);
+//             }
+//             else{
+//                 break;
+//             }
+//             pq.pop();
+//             turns++;
+//             k--;
+//         }
+
+//         return happy;
+//     }
+// };
 class Solution {
 public:
-    long long maximumHappinessSum(vector<int>& happiness, int k) {
-        int n=happiness.size();
-
-        priority_queue<int> pq;
-        for(int i=0;i<n;i++){
-            pq.push(happiness[i]);
-        }
-
-        int turns=0;
-        long long happy=0;
-        while(!pq.empty() && k>0){
-            int h=pq.top();
-            if(h-turns>0){
-                happy+=(h-turns);
-            }
-            else{
+    long long maximumHappinessSum(vector<int>& christmasJoy, int gifts) {
+       
+        sort(christmasJoy.begin(), christmasJoy.end(), greater<int>());
+        
+        long long totalJoy = 0;
+        for (int turn = 0; turn < gifts; ++turn) {
+            int currentJoy = christmasJoy[turn] - turn;
+            if (currentJoy <= 0)
                 break;
-            }
-            pq.pop();
-            turns++;
-            k--;
+            totalJoy += currentJoy;
         }
-
-        return happy;
+        
+        return totalJoy;
     }
 };
