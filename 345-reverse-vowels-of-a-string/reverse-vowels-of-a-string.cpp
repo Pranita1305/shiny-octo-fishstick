8afc8843fd975e45e72348b4dbf61c6d60fd0504
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2025-12-26 17:30:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (32.67%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,26 +1,60 @@
+// class Solution {
+// public:
+//     string reverseVowels(string s) {
+//         int n=s.size();
+//         int l=0;
+//         int r=n-1;
+
+//         set<char> vowels={'a','e','i','o','u','A','E','I','O','U'};
+
+//         while(l<r){
+//             if(vowels.find(s[l])!=vowels.end() && vowels.find(s[r])!=vowels.end()){
+//                 swap(s[l],s[r]);
+//                 l++;
+//                 r--;
+//             }
+//             else if(vowels.find(s[l])!=vowels.end()){
+//                 r--;
+//             }
+//             else{
+//                 l++;
+//             }
+//         }
+
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
     string reverseVowels(string s) {
-        int n=s.size();
-        int l=0;
-        int r=n-1;
-
-        set<char> vowels={'a','e','i','o','u','A','E','I','O','U'};
-
-        while(l<r){
-            if(vowels.find(s[l])!=vowels.end() && vowels.find(s[r])!=vowels.end()){
-                swap(s[l],s[r]);
-                l++;
-                r--;
+        // Convert the input string to a character array.
+        string word = s;
+        int start = 0;
+        int end = s.length() - 1;
+        string vowels = "aeiouAEIOU";
+        
+        // Loop until the start pointer is no longer less than the end pointer.
+        while (start < end) {
+            // Move the start pointer towards the end until it points to a vowel.
+            while (start < end && vowels.find(word[start]) == string::npos) {
+                start++;
             }
-            else if(vowels.find(s[l])!=vowels.end()){
-                r--;
+            
+            // Move the end pointer towards the start until it points to a vowel.
+            while (start < end && vowels.find(word[end]) == string::npos) {
+                end--;
             }
-            else{
-                l++;
-            }
+            
+            // Swap the vowels found at the start and end positions.
+            swap(word[start], word[end]);
+            
+            // Move the pointers towards each other for the next iteration.
+            start++;
+            end--;
         }
-
-        return s;
+        
+        // Return the modified string.
+        return word;
     }
 };
