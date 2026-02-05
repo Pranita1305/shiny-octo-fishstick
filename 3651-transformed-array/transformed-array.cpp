74584d58958c42b48db7bd3a751567f993a99d38
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2026-02-05 10:16:51 +0530

    Time: 8 ms (46.52%) | Memory: 25.6 MB (91.36%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -5,14 +5,15 @@ public:
         int n=nums.size();
 
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                res.push_back(nums[i]);
-            }
-            else{
-                int index=(nums[i]+i)%n;
-                if(index<0) index+=n;
-                res.push_back(nums[index]);
-            }
+            // if(nums[i]==0){
+            //     res.push_back(nums[i]);
+            // }
+            // else{
+            //     int index=(nums[i]+i)%n;
+            //     if(index<0) index+=n;
+            //     res.push_back(nums[index]);
+            // }
+            res.push_back(nums[((i + nums[i]) % n + n) % n]);
         }
 
         return res;
