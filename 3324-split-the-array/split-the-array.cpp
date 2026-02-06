852b17b7fde08c3234f2f4fdfb1243a3ad9a980d
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2026-02-06 23:02:28 +0530

    Time: 3 ms (38.57%) | Memory: 25.1 MB (60.89%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> freq(101,0);
+
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+            if(freq[nums[i]]>2) return false;
+        }
+        return true;
+    }
+};
