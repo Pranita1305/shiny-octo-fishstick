198fd2a13106deedb5a8fbf22c020664f62a36bd
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2026-02-10 21:57:37 +0530

    Time: 142 ms (96.86%) | Memory: 35.9 MB (99.79%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,24 @@
+// pmr::unordered_map version  
+#include <memory_resource>
+pmr::unsynchronized_pool_resource pool;
+pmr::unordered_map<int, int> freq(&pool);
+class Solution {
+public:
+    static int longestBalanced(vector<int>& nums) {
+        const int n=nums.size();
+        freq.reserve(n);
+        int len=0;
+        for(int l=0; l<n; l++){
+            if (l>n-len) break;// len cannot be longer
+            int cnt[2]={0};
+            for(int r=l; r<n; r++){
+                const int x=nums[r];
+                if (++freq[x]==1) cnt[x&1]++;
+                if (cnt[0]==cnt[1])
+                    len=max(len, r-l+1);
+            }
+            freq.clear();
+        }
+        return len;
+    }
+};
