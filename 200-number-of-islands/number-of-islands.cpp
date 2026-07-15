3b7d6dd271b1014d49ef76a98b2b7cd106abbef0
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2026-07-15 23:03:53 +0530

    Time: 20 ms (94.26%) | Memory: 16.1 MB (96.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,43 +1,58 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int islands = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
-        unordered_set<string> visited;
+        int n=grid.size();
+        int m=grid[0].size();
 
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int count=0;
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1' && visited.find(to_string(r) + "," + to_string(c)) == visited.end()) {
-                    islands++;
-                    bfs(grid, r, c, visited, directions, rows, cols);
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+                if(grid[r][c]=='1'){
+                    count++;
+                    dfs(grid,r,c);
                 }
             }
         }
 
-        return islands;        
-    }
+        
 
+        return count;
+        
+    }
 private:
-    void bfs(vector<vector<char>>& grid, int r, int c, unordered_set<string>& visited, vector<pair<int, int>>& directions, int rows, int cols) {
-        queue<pair<int, int>> q;
-        visited.insert(to_string(r) + "," + to_string(c));
-        q.push({r, c});
+    // void bfs(vector<vector<char>>& grid, int r, int c,unordered_set<string>& vis,vector<pair<int,int>>& dir, int n, int m){
+    //     queue<pair<int,int>> q;
+    //     q.push({r,c});
 
-        while (!q.empty()) {
-            auto [row, col] = q.front();
-            q.pop();
+    //     vis.insert({to_string(r)+","+to_string(c)});
 
-            for (auto [dr, dc] : directions) {
-                int nr = row + dr;
-                int nc = col + dc;
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && visited.find(to_string(nr) + "," + to_string(nc)) == visited.end()) {
-                    q.push({nr, nc});
-                    visited.insert(to_string(nr) + "," + to_string(nc));
-                }
-            }
+    //     while(!q.empty()){
+    //         auto [row,col]=q.front();
+    //         q.pop();
+
+    //         for(auto [dr,dc]:dir){
+    //             int nr=r+dr;
+    //             int nc=c+dc;
+
+    //             if(nr>=0 && nr<n && nc>=0 && nc<m){
+    //                 q.push({nr,nc});
+    //                 vis.insert(to_string(nr) + "," + to_string(nc));
+    //             }
+    //         }
+    //     }
+    // }
+
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
+            return;
         }
+        
+        grid[i][j] = '0'; // mark as visited
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
     }
-};    
+    
+};
