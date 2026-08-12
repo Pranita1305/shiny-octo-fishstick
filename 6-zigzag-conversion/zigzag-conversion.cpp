f2d1278f6a586862d64088eaabb73b9b4c66ea25
Pranita1305 <151372019+Pranita1305@users.noreply.github.com>
2026-08-12 14:06:33 +0530

    Time: 3 ms (76.76%) | Memory: 14.2 MB (24.83%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -8,41 +8,55 @@ public:
             return s;
         }
 
-        vector<vector<char>> pattern(numRows);
-        int i = 0;
-        int j = 0;
-        bool flag = true; // true indicates moving down, false indicates moving up
+        vector<string> pattern(numRows,"");
 
-        while (i < n) {
-            if (flag) {
-                // Move downward from row 0 to numRows - 1
-                while (i < n && j < numRows) {
-                    pattern[j].push_back(s[i]);
-                    i++;
-                    j++;
-                }
-                // Prepare for the upward diagonal move
-                j = numRows - 2;
-                flag = false;
-            } else {
-                // Move upward from row numRows - 2 down to row 1
-                while (i < n && j > 0) {
-                    pattern[j].push_back(s[i]);
-                    i++;
-                    j--;
-                }
-                // Prepare for the downward move starting at row 0
-                j = 0;
-                flag = true;
+        int dir=-1;
+        int j=0;
+
+        for(int i=0;i<n;i++){
+            if(j==numRows-1 || j==0){
+                if(dir==1) dir=-1;
+                else dir=1;
             }
+            pattern[j]+=s[i];
+            if(dir==1) j++;
+            else j--;
+
         }
+
+        // vector<vector<char>> pattern(numRows);
+        // int i = 0;
+        // int j = 0;
+        // bool flag = true; // true indicates moving down, false indicates moving up
+
+        // while (i < n) {
+        //     if (flag) {
+        //         // Move downward from row 0 to numRows - 1
+        //         while (i < n && j < numRows) {
+        //             pattern[j].push_back(s[i]);
+        //             i++;
+        //             j++;
+        //         }
+        //         // Prepare for the upward diagonal move
+        //         j = numRows - 2;
+        //         flag = false;
+        //     } else {
+        //         // Move upward from row numRows - 2 down to row 1
+        //         while (i < n && j > 0) {
+        //             pattern[j].push_back(s[i]);
+        //             i++;
+        //             j--;
+        //         }
+        //         // Prepare for the downward move starting at row 0
+        //         j = 0;
+        //         flag = true;
+        //     }
+        // }
 
         // Reconstruct the final string from the pattern rows
         string res = "";
-        for (int r = 0; r < numRows; r++) {
-            for (char ch : pattern[r]) {
-                res += ch;
-            }
+        for(string s:pattern){
+            res+=s;
         }
 
         return res;
